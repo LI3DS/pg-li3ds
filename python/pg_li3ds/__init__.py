787c9061eb2521d1e46ccbd8c521c27d103d3f56
Éric Lemoine <eric.lemoine@oslandia.com>
2017-08-25 10:46:01 +0200

    Add transform functions for points
    This adds functions for transforming points, which are represented as arrays. I am adding these functions mainly for testing purpose. They canonical function is implemented based on its box4d counterpart.

--- python/pg_li3ds/__init__.py
+++ python/pg_li3ds/__init__.py
@@ -362,6 +362,42 @@ def transform_box4d_config(box4d, config, source, target, time):
     return transform_box4d_list(box4d, transforms, time)
 
 
+def _transform_point(point, func_name, func_sign, params):
+    ''' Transform the point, using func_name, func_sign and params.
+    '''
+    point_str = ' '.join(map(str, point))
+    box4d = 'BOX4D({point_str},{point_str})'.format(point_str=point_str)
+    box4d_out = _transform_box4d(box4d, func_name, func_sign, params)
+    point_out = list(map(float, box4d_out[6:-1].split(',')[0].split(' ')))
+    return point_out
+
+
+def transform_point_one(point, transfoid, time):
+    ''' Transform the point, using transfoid and time. time is ignored if the transform
+        is static.
+    '''
+    transfo = get_transform(transfoid, time)
+    if not transfo:
+        return None
+    name, params, func_name, func_sign = transfo
+    plpy.log('apply transfo "{}" (function: "{}") to point'.format(name, func_name))
+    return _transform_point(point, func_name, func_sign, params)
+
+
+def transform_point_list(point, transfoids, time):
+    ''' Transform the point, using all the transforms in the transfoids list. '''
+    for transfoid in transfoids:
+        point = transform_point_one(point, transfoid, time)
+    return point
+
+
+def transform_point_config(point, config, source, target, time):
+    ''' Apply the transform path from "source" to "target" for the provided "config".
+    '''
+    transforms = dijkstra(config, source, target)
+    return transform_point_list(point, transforms, time)
+
+
 def _transform_patch(patch, func_name, func_sign, params):
     ''' Transform the patch, using func_name, func_sign and params.
     '''
